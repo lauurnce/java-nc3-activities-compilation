4dc2dc7a745ae36dcd749b0a33edd5d834929c6f
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 00:22:13 +0800

    Initial commit

---
+++ Activities/Activity 12/Reptile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.polymorphismactivity.day27;
+
+/**
+ *
+ * @author Admin
+ */
+public class Reptile extends Animal {
+    @Override
+    public void animalDiet(String diet) {
+        System.out.println("Reptile diet: " + diet);
+    }
+}
