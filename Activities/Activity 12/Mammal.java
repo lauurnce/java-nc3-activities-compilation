65e3851be82e4fa49f8f5d1365bf209efe5e1187
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 00:21:52 +0800

    Initial commit

---
+++ Activities/Activity 12/Mammal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.polymorphismactivity.day27;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mammal extends Animal {
+     @Override
+    public void animalDiet(String diet) {
+        System.out.println("Mammal diet: " + diet);
+    }
+
+    public static boolean isHumanClass(boolean x) {
+        return x;
+    }
+}
