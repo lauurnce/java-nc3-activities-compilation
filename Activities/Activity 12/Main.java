125850896d79973dcc84ad76483d72ef7b91b193
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 08:59:45 +0800

    Polymorphism

--- Activities/Activity 12/Main.java
+++ Activities/Activity 12/Main.java
@@ -10,34 +10,32 @@ package com.joysistvi.polymorphismactivity.day27;
  * @author Admin
  */
 public class Main {
+
     public static void main(String[] args) {
-        // Polymorphism in action
-        Animal a1 = new Mammal();
-        Animal a2 = new Reptile();
-        Animal a3 = new Bird();
-        Animal a4 = new Amphibian();
-        Animal a5 = new Fish();
 
-        // Diets
-        a1.animalDiet("plants and meat");
-        a2.animalDiet("insects");
-        a3.animalDiet("seeds and worms");
-        a4.animalDiet("small insects");
-        a5.animalDiet("plankton");
+        Animal mm = new Mammal();
+        Animal rp = new Reptile();
+        Animal bd = new Bird();
+        Animal ap = new Amphibian();
+        Animal fs = new Fish();
 
-        System.out.println();
+        System.out.println("======ANIMALS INFORMATION====");
+        System.out.printf("%-12s | %s%n", "Animal", "Diet");
+        System.out.println("-----------------------------");
+        mm.animalDiet("plants and meat");
+        rp.animalDiet("insects");
+        bd.animalDiet("seeds and worms");
+        ap.animalDiet("small insects");
+        fs.animalDiet("plankton");
 
-        // Temperatures
-        a1.temperature(); // Mammal inherits default
-        a2.temperature(); // Reptile inherits default
-        a3.temperature(); // Bird override
-        a4.temperature(); // Amphibian override
-        a5.temperature(); // Fish override
+        System.out.println("\n=============================");
+        System.out.printf("%-12s | %s%n", "Animal", "Temperature");
+        System.out.println("-----------------------------");
+        bd.temperature();
+        ap.temperature();
+        fs.temperature();
 
-        System.out.println();
-
-        // Mammal static method
-        System.out.println("Is Human Class? " + Mammal.isHumanClass(true));
+        System.out.println("\nQUESTION/S: ");
+        System.out.println("Is Human considered a mammal? " + Mammal.isHumanClass(true));
     }
 }
- 
